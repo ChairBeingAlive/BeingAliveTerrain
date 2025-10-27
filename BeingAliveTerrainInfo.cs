4feba559ffd826368c8a4be01a39c4cf41d1fa24
xTree <xliotx@gmail.com>
2025-10-27 21:30:30 +0100

    test v0.1.3 workflow.

--- BeingAliveTerrainInfo.cs
+++ BeingAliveTerrainInfo.cs
@@ -5,13 +5,14 @@ using Grasshopper.Kernel;
 
 namespace BeingAliveTerrain {
   public class BeingAliveTerrainInfo : GH_AssemblyInfo {
-    public override string Name => "BAT";
+    public override string Name => "BeingAliveTerrain";
 
     // Return a 24x24 pixel bitmap to represent this GHA library.
     public override Bitmap Icon => null;
 
     // Return a short string describing the purpose of this GHA library.
-    public override string Description => "";
+    public override string Description =>
+        "Terrain Plugin developed at the Chair of Being Alive @ ETH Zurich.";
 
     public override Guid Id => new Guid("d3b8edbb-54d2-4d80-ac59-a04b9f77f1dd");
 
@@ -21,7 +22,10 @@ namespace BeingAliveTerrain {
     // Return a string representing your preferred contact details.
     public override string AuthorContact => "zhma@ethz.ch";
 
-    // Return a string representing the version.  This returns the same version as the assembly.
-    public override string AssemblyVersion => GetType().Assembly.GetName().Version.ToString();
+    // public override string AssemblyVersion => GetType().Assembly.GetName().Version.ToString();
+    public override string AssemblyVersion => "0.1.3";
+
+    // this is currently the variable used by McNeel for plugin system
+    public override string Version => AssemblyVersion;
   }
 }
