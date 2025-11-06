a87780233bad187b3a95be3bef9975404d18dab5
xTree <xliotx@gmail.com>
2025-11-06 13:22:09 +0100

    add icons for slopeanalysis.

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -119,5 +119,15 @@ namespace BeingAliveTerrain.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap slopeAnalysis {
+            get {
+                object obj = ResourceManager.GetObject("slopeAnalysis", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
